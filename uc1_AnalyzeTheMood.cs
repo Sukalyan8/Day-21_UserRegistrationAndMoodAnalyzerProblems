70dc8cb08d4eea503207acca43ac7ee340e60b6d
Sukalyan <sukalyandash1920@gmail.com>
2022-02-01 18:12:34 +0530

    Check Whether Mood is Happy or Sad

---
+++ uc1_AnalyzeTheMood.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerException
+{
+    public class uc1_AnalyzeTheMood
+    {
+        string message;
+        public void MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            if (message.ToLower().Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
