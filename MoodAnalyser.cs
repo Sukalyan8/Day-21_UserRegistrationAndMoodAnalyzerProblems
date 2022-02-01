daa74f6952b394441ce9c961aa0cf797faeabf8f
Sukalyan <sukalyandash1920@gmail.com>
2022-02-01 18:06:04 +0530

    Using Test Cases Mood Analyzer

---
+++ MoodAnalyser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TestProject2
+{
+    internal class MoodAnalyser
+    {
+        public MoodAnalyser()
+        {
+        }
+
+        public MoodAnalyser(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; }
+
+        internal void AnalyzeMood()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
