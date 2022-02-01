d5f3b163c070f713e3fda4aac3c1c2a4a4487e51
Sukalyan <sukalyandash1920@gmail.com>
2022-02-01 18:08:33 +0530

    Using Mood AnalyzerFactory class

---
+++ MoodAnalyserFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TestProject2
+{
+    internal class MoodAnalyserFactory
+    {
+        internal string InvokeAnalyserMethod(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object CreateMoodAnalyserParameterizedObject(string v1, string v2, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object CreateMoodAnalyserObject(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
